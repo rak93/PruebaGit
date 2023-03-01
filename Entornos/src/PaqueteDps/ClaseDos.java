81dda9c83806f7a85dc9e45def2ac64a929ed7d4
b14-01t <b14-01t@AULAB14-PC01.CICLOS.local>
2023-03-01 16:43:46 +0100

    primera

---
+++ Entornos/src/PaqueteDps/ClaseDos.java
@@ -0,0 +1,21 @@
+package PaqueteDps;
+
+public class ClaseDos {
+	private int numero;
+	private String orden;
+
+
+	public ClaseDos(int numero, String orden) {
+		super();
+		this.numero = numero;
+		this.orden = orden;
+	}
+
+
+	public void restar() {
+		int suma;
+		suma =numero - 1;
+	}
+
+
+}
---
+++ Entornos/src/PaqueteUno/ClasePrimera.java
@@ -0,0 +1,18 @@
+package PaqueteUno;
+
+public class ClasePrimera {
+	private int num;
+	private String nombre;
+
+	public ClasePrimera(int num, String nombre) {
+		
+		this.num = num;
+		this.nombre = nombre;
+	}
+
+	public void sumar() {
+		int suma;
+		suma = num + 1;
+	}
+
+}
